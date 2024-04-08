2a9661342fc05a954d711580c180c20220de4731
Manu <manu1998kj@gmail.com>
2024-04-08 20:56:18 +0100

    Update manifest.json and add service worker for caching resources

---
+++ public/sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'Plant Recognition v1.0';
+
+console.log('Service Worker: Registered');
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+    console.log('Service Worker: Installing....');
+    event.waitUntil((async () => {
+
+        console.log('Service Worker: Caching App Shell at the moment......');
+        try {
+            const cache = await caches.open(CACHE_NAME);
+            cache.addAll([
+                '/',
+                '/css/output.css',
+            ]);
+        }
+        catch{
+            console.log("error occured while caching...")
+        }
+
+    })());
+});
+
+//clear cache on reload
+self.addEventListener('activate', event => {
+// Remove old caches
+    event.waitUntil(
+        (async () => {
+            const keys = await caches.keys();
+            return keys.map(async (cache) => {
+                if(cache !== cacheName) {
+                    console.log('Service Worker: Removing old cache: '+cache);
+                    return await caches.delete(cache);
+                }
+            })
+        })()
+    )
+})
+
+self.addEventListener('fetch', function(event) {
+
+    console.log('Service Worker: Fetch', event.request.url);
+
+    console.log("Url", event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
