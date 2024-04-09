e14303b61324a0d71368761ee03c0bbaa0ff61b9
Manu <manu1998kj@gmail.com>
2024-04-09 22:53:11 +0100

    added service worker

--- public/sw.js
+++ public/sw.js
@@ -1,52 +1,50 @@
-const CACHE_NAME = 'Plant Recognition v1.0';
+const CACHE_NAME = "Plant Recognition v1.0";
 
-console.log('Service Worker: Registered');
+console.log("Service Worker: Registered");
+
+function log(message) {
+  console.log("Service Worker: " + message);
+}
 
 // Use the install event to pre-cache all initial resources.
-self.addEventListener('install', event => {
-    console.log('Service Worker: Installing....');
-    event.waitUntil((async () => {
-
-        console.log('Service Worker: Caching App Shell at the moment......');
-        try {
-            const cache = await caches.open(CACHE_NAME);
-            cache.addAll([
-                '/',
-                '/css/output.css',
-            ]);
-        }
-        catch{
-            console.log("error occured while caching...")
-        }
-
-    })());
+self.addEventListener("install", (event) => {
+  log("Service Worker: Installing....");
+  event.waitUntil(
+    (async () => {
+      log("Service Worker: Caching App Shell at the moment......");
+      try {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(["/", "/css/output.css"]);
+      } catch {
+        log("error occured while caching...");
+      }
+    })()
+  );
 });
 
 //clear cache on reload
-self.addEventListener('activate', event => {
-// Remove old caches
-    event.waitUntil(
-        (async () => {
-            const keys = await caches.keys();
-            return keys.map(async (cache) => {
-                if(cache !== cacheName) {
-                    console.log('Service Worker: Removing old cache: '+cache);
-                    return await caches.delete(cache);
-                }
-            })
-        })()
-    )
-})
+self.addEventListener("activate", (event) => {
+  // Remove old caches
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      return keys.map(async (cache) => {
+        if (cache !== cacheName) {
+          log(cache);
+          return await caches.delete(cache);
+        }
+      });
+    })()
+  );
+});
 
-self.addEventListener('fetch', function(event) {
-
-    console.log('Service Worker: Fetch', event.request.url);
-
-    console.log("Url", event.request.url);
-
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", function (event) {
+  log(event.request.url);
+  log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      log(response);
+      return response || fetch(event.request);
+    })
+  );
 });
