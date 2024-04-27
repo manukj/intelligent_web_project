347b70081931aea1e37ac5e51ba3239186ea5b7d
Manu <manu1998kj@gmail.com>
2024-04-27 11:46:03 +0100

    Fix offline syncing issue and update chat UI

--- public/javascripts/chat_script.js
+++ public/javascripts/chat_script.js
@@ -9,6 +9,22 @@ function init() {
   getChatHistory(plantId);
   registerSocket();
   registerFormSubmit();
+  listenForOnlineSync();
+}
+
+function listenForOnlineSync() {
+  window.addEventListener("online", function () {
+    console.log("You are now online.");
+    openSyncChatsIDB().then((db) => {
+      getAllSyncChatMessages(db).then((syncChats) => {
+        syncChats.forEach((data) => {
+          console.log("syncing data offline chat", data.value);
+          socket.emit("chat", data.value);
+          deleteSyncChatFromIDB(db, data.id);
+        });
+      });
+    });
+  });
 }
 
 function registerFormSubmit() {
@@ -48,14 +64,6 @@ function sendMessage() {
       }
     });
   });
-
-  // if (navigator.onLine) {
-  //   console.log("Form submitted");
-  //   socket.emit("chat");
-  //   input.value = "";
-  // } else {
-  //   input.value = "";
-  // }
 }
 
 function registerSocket() {
@@ -145,25 +153,6 @@ function createChatMessageElement(message) {
       </div>
     </div>
   `;
-
-  function generateRandomColor(name) {
-    const colors = [
-      "#FF5733",
-      "#C70039",
-      "#900C3F",
-      "#581845",
-      "#FFC300",
-      "#DAF7A6",
-      "#FF5733",
-      "#C70039",
-      "#900C3F",
-      "#581845",
-      "#FFC300",
-      "#DAF7A6",
-    ];
-    const index = name.charCodeAt(0) + (name.charCodeAt(1) % colors.length);
-    return colors[index];
-  }
 
   const chatHeaderDiv = document.createElement("div");
   chatHeaderDiv.classList.add("chat-header");
