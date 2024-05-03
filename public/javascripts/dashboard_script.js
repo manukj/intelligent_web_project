8b81f9c00e8118d8e32fcf9600932b2c53d1fa34
Manu <manu1998kj@gmail.com>
2024-05-03 15:40:38 +0100

    Refactor dashboard_script.js and dashboard.ejs for improved functionality and user experience

--- public/javascripts/dashboard_script.js
+++ public/javascripts/dashboard_script.js
@@ -1,3 +1,7 @@
 function showDetailsPage(id) {
-  window.location.href = "/details/" + id;
+  window.location.href = "details/" + id+"/Mark";
+}
+
+function openAddPlantPage() {
+  window.location.href = "/addPlant";
 }
