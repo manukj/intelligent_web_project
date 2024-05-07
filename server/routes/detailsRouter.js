beb665fcbb4d171d3bb10d451ded5d3a69c92690
acs22js <jsong57@sheffield.ac.uk>
2024-05-07 23:12:09 +0100

    Changes

--- server/routes/detailsRouter.js
+++ server/routes/detailsRouter.js
@@ -1,10 +1,12 @@
 var express = require("express");
-const { detailsPage, savePlant} = require("../controllers/detailsController");
+const { detailsPage, savePlant, searchPlant} = require("../controllers/detailsController");
 const plantModel = require("../models/plant");
 var router = express.Router();
 
 router.get("/:plant_id/:user_name", detailsPage);
 
 router.post('/save', savePlant);
+
+router.get('/search',searchPlant);
 
 module.exports = router;
