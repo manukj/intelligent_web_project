985ee8d4c7b78a2e87ea7f7778c00c681296077b
Manu <manu1998kj@gmail.com>
2024-05-15 00:38:20 +0100

    chore: Refactor file paths and naming conventions in addPlantRouter and multer.config

--- server/controllers/addPlantController.js
+++ server/controllers/addPlantController.js
@@ -1,20 +1,20 @@
-const AddPlant = require('../models/add_plant_model'); // Import the AddPlant model
+const AddPlant = require("../models/add_plant_model"); // Import the AddPlant model
 
 exports.addAPlantPage = async (req, res, next) => {
   res.render("add_plant/add_plant", { title: "Add A Plant" });
+};
+
+exports.addImage = async (req, res, next) => {
+  console.log("req.file", req.file);
+  res.json({ message: "Image uploaded successfully" });
 };
 
 exports.addNewPlantToDb = async (req, res, next) => {
   const plantData = req.body;
   let photoPath = null;
 
-  // Check if a file was uploaded and if it has a valid filename
-  console.log("testt------")
-  console.log(req.file);
-  // console.log(req.file.filename);
-  // console.log(req.file.path);
-  if (req.file && req.file.filename) {
-    photoPath = req.file.filename; // Store the image file name
+  if (req.file && req.file.path) {
+    photoPath = req.file.path; // Store the image file name
   }
   const newPlant = new AddPlant({
     plantName: plantData.plantName,
@@ -31,17 +31,17 @@ exports.addNewPlantToDb = async (req, res, next) => {
 
   console.log("Adding new plant: ", newPlant);
   return newPlant
-      .save()
-      .then((plant) => {
-        console.log("Plant added successfully! :", plant);
-        res.json(plant);
-      })
-      .catch((err) => {
-        console.error("Error adding plant: ", err);
-        return res.status(500).json({
-          success: false,
-          message: "Error adding plant",
-          error: err.message,
-        });
+    .save()
+    .then((plant) => {
+      console.log("Plant added successfully! :", plant);
+      res.json(plant);
+    })
+    .catch((err) => {
+      console.error("Error adding plant: ", err);
+      return res.status(500).json({
+        success: false,
+        message: "Error adding plant",
+        error: err.message,
       });
+    });
 };
--- server/routes/addPlantRouter.js
+++ server/routes/addPlantRouter.js
@@ -1,11 +1,14 @@
 var express = require("express");
-const { addAPlantPage, addNewPlantToDb } = require("../controllers/addPlantController");
-const upload = require('./multer.config'); // Import the multer configuration
+const {
+  addAPlantPage,
+  addNewPlantToDb,
+} = require("../controllers/addPlantController");
+const upload = require("./multer.config");
 
 var router = express.Router();
 
 router.get("/", addAPlantPage);
 
-router.post("/addNewPlant", upload.single('photo'), addNewPlantToDb); // Add the multer middleware
+router.post("/addNewPlant", upload.single("photo"), addNewPlantToDb);
 
 module.exports = router;
