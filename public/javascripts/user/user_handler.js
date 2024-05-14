1bb9b2094e4cc4a4405be29a294db85f90a2a520
Manu <manu1998kj@gmail.com>
2024-05-14 08:12:44 +0100

    Update header component with user login functionality and logout button

--- public/javascripts/user/user_handler.js
+++ public/javascripts/user/user_handler.js
@@ -1,39 +1,56 @@
 const USERS_STORE_NAME = "users";
 const USER_IDB_NAME = "userIDB";
 
-function logout(db) {
+function logout() {
   return new Promise((resolve, reject) => {
-    const transaction = db.transaction([USERS_STORE_NAME], "readwrite");
-    const objectStore = transaction.objectStore(USERS_STORE_NAME);
+    initializeUserIDB().then((db) => {
+      const transaction = db.transaction([USERS_STORE_NAME], "readwrite");
+      const objectStore = transaction.objectStore(USERS_STORE_NAME);
 
-    const request = objectStore.clear();
+      const request = objectStore.clear();
 
-    request.onsuccess = function () {
-      console.log("User logged out successfully.");
-      resolve();
-    };
+      request.onsuccess = function () {
+        console.log("User logged out successfully.");
+        resolve();
+        showLoginUserModel();
+      };
 
-    request.onerror = function (event) {
-      console.error("Error logging out:", event.target.error);
-      reject(event.target.error);
-    };
+      request.onerror = function (event) {
+        console.error("Error logging out:", event.target.error);
+        reject(event.target.error);
+      };
+    });
   });
 }
 
-function loggedInUser(username, db) {
+function logInUser() {
+  var loginInTextField = document.getElementById("user_name");
+  var userName = loginInTextField.value;
+  loginInTextField.value = "";
   return new Promise((resolve, reject) => {
-    const transaction = db.transaction([USERS_STORE_NAME], "readwrite");
-    const objectStore = transaction.objectStore(USERS_STORE_NAME);
-
-    const request = objectStore.add(username);
-    request.onsuccess = function () {
-      console.log("User logged in successfully.");
-      resolve();
-    };
-    request.onerror = function (event) {
-      console.error("Error logging in:", event.target.error);
-      reject(event.target.error);
-    };
+    initializeUserIDB()
+      .then((db) => {
+        const transaction = db.transaction([USERS_STORE_NAME], "readwrite");
+        const userStore = transaction.objectStore(USERS_STORE_NAME);
+        const addRequest = userStore.add({ value: userName });
+        addRequest.addEventListener("success", () => {
+          console.log("Added " + "#" + addRequest.result + ": " + userName);
+          const getRequest = userStore.get(addRequest.result);
+          getRequest.addEventListener("success", () => {
+            resolve(getRequest.result);
+            hideLoginUserModel();
+          });
+          getRequest.addEventListener("error", (event) => {
+            reject(event.target.error);
+          });
+        });
+        addRequest.addEventListener("error", (event) => {
+          reject(event.target.error);
+        });
+      })
+      .catch((error) => {
+        reject(error);
+      });
   });
 }
 
@@ -59,6 +76,10 @@ function initializeUserIDB() {
   return new Promise((resolve, reject) => {
     const request = indexedDB.open(USER_IDB_NAME, 1);
 
+    request.onerror = function (event) {
+      reject(new Error(`Database error: ${event.target}`));
+    };
+
     request.onupgradeneeded = function (event) {
       const db = event.target.result;
       db.createObjectStore(USERS_STORE_NAME, {
@@ -72,10 +93,53 @@ function initializeUserIDB() {
       console.log("Database initialized successfully.");
       resolve(db);
     };
-
-    request.onerror = function (event) {
-      console.error("Error initializing database:", event.target.error);
-      reject(event.target.error);
-    };
   });
+}
+
+function checkIfUserLoggedIn() {
+  return new Promise((resolve, reject) => {
+    initializeUserIDB()
+      .then((db) => {
+        getUserName(db)
+          .then((userName) => {
+            if (userName) {
+              resolve(userName);
+            } else {
+              showLoginUserModel();
+            }
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+function showLoginUserModel() {
+  var loginUserModel = document.getElementById("login_user_model");
+  if (loginUserModel) {
+    loginUserModel.showModal();
+    hideLogoutButton();
+  }
+}
+
+function hideLoginUserModel() {
+  var loginUserModel = document.getElementById("login_user_model");
+  if (loginUserModel) {
+    loginUserModel.close();
+    showLogoutButton();
+  }
+}
+
+function hideLogoutButton() {
+  var logout_button = document.getElementById("logout_button");
+  logout_button.classList.add("hidden");
+}
+
+function showLogoutButton() {
+  var logout_button = document.getElementById("logout_button");
+  logout_button.classList.remove("hidden");
 }
