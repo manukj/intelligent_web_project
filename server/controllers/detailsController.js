4afba70561480e5ef86d4e189b61ee58ad65a592
acs22js <jsong57@sheffield.ac.uk>
2024-05-15 04:00:10 +0100

    Changes

--- server/controllers/detailsController.js
+++ server/controllers/detailsController.js
@@ -1,12 +1,34 @@
 const plantModel = require("../models/plant");
 const { ObjectId } = require("mongodb");
+const { response } = require("express");
+const AddPlant = require("../models/add_plant_model");
+async function getPlant(plant_id) {
+  try{
+    const plant = AddPlant.findOne({_id:plant_id});
+    return plant;
+  }catch (e) {
+    return null;
+  }
+}
+
 exports.detailsPage = async (req, res, next) => {
   const plantModel = require("../models/plant");
   const user_name = req.params.user_name;
+  console.log(req.params.plant_id)
+  const plant = await getPlant(req.params.plant_id);
+  console.log(plant);
+  var dbpediaResult = await getPlantDetails(plant.plantName);
   try {
-    const data = await plantModel.findById(req.params.plant_id);
+    const data = await getPlant(req.params.plant_id);
+    console.log("1");
     console.log(data);
-    res.render("details/details", { data, user_name: user_name });
+    console.log("2");
+    console.log(dbpediaResult)
+    res.render("details/details", {
+      data,
+      user_name: user_name,
+      dbpediaResult: dbpediaResult,
+    });
   } catch (e) {
     console.error(e);
     res.status(404).send();
@@ -21,4 +43,48 @@ exports.savePlant = async (req, res, next) => {
     console.log(e);
     res.status(500).send(e);
   }
+};
+
+async function getPlantDetails(plantName) {
+  const resource = `http://dbpedia.org/resource/${encodeURIComponent(
+      plantName
+  )}`;
+  const endpointUrl = "https://dbpedia.org/sparql";
+
+  const sqarqlQuery = `
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    
+    SELECT ?name ?description WHERE {
+      <${resource}> rdfs:label ?name .
+      <${resource}> rdfs:comment ?description .
+      FILTER (langMatches(lang(?name), "en")) .
+      FILTER (langMatches(lang(?description), "en")) .
+    }`;
+
+  const encodedQuery = encodeURIComponent(sqarqlQuery);
+  const url = `${endpointUrl}?query=${encodedQuery}&format=json`;
+  var result;
+  try {
+    result = await fetch(url).then((response) => response.json());
+    console.log(JSON.stringify(result));
+  } catch (error) {
+    console.log(error);
+  }
+  return result;
+}
+
+exports.searchPlant = async (req, res, next) => {
+  fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results.bindings.length > 0) {
+          let bindings = data.results.bindings;
+          let result = JSON.stringify(bindings);
+          console.log(data);
+          console.log(result);
+        } else {
+        }
+      })
+      .catch((error) => {});
 };
--- server/routes/detailsRouter.js
+++ server/routes/detailsRouter.js
@@ -1,10 +1,12 @@
 var express = require("express");
-const { detailsPage, savePlant} = require("../controllers/detailsController");
-const plantModel = require("../models/plant");
+const { detailsPage, savePlant, searchPlant} = require("../controllers/detailsController");
+
 var router = express.Router();
 
 router.get("/:plant_id/:user_name", detailsPage);
 
 router.post('/save', savePlant);
+
+router.get('/search',searchPlant);
 
 module.exports = router;
