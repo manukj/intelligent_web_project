4fcac5cbeccfb9c47db67cee3e6135f43cb91073
Manu <manu1998kj@gmail.com>
2024-05-16 04:48:56 +0100

    chore: Improve image upload and display functionality

--- server/routes/multer.config.js
+++ server/routes/multer.config.js
@@ -7,7 +7,8 @@ const storage = multer.diskStorage({
     cb(null, "public/images/uploads/"); // Specify the upload folder
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + path.extname(file.originalname));
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + uniqueSuffix + path.extname(file.originalname));
   },
 });
 
